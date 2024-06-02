efe87ea7b8c7321d5ddf77ec672ebf15fafb180a
DeployingSites <kumarsinhaakshat19@gmail.com>
2024-06-02 11:30:40 +0530

    commit

---
+++ 04controlFlow/switch.js
@@ -0,0 +1,26 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("Janurary")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("march")
+        break;
+
+    default:
+        break;
+}
